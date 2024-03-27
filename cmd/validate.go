1b2423a938841c7501545d304634292e04e120c7
dylan <dwelch@aerospike.com>
2024-03-27 09:58:13 -0700

    don't check nil validation errors

--- cmd/validate.go
+++ cmd/validate.go
@@ -93,7 +93,7 @@ func newValidateCmd() *cobra.Command {
 			verrs, err := conf.NewConfigValidator(asconfig, mgmtLibLogger, version).Validate()
 			// verrs is an empty slice if err is not nil but no
 			// validation errors were found
-			if len(verrs.Errors) > 0 {
+			if verrs != nil && len(verrs.Errors) > 0 {
 				// force validation errors to be written to stdout
 				// so they can more easily be grepd etc.
 				cmd.Print(verrs.Error())
