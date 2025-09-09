cf64799ac9ef6cc8d6295a651d707328c05c0b33
grant <grant@iowntheinter.net>
2025-09-09 05:34:59 +0000

    install go to /usr/local/bin

--- .github/docker/install_deps.sh
+++ .github/docker/install_deps.sh
@@ -5,6 +5,7 @@ function install_deps_debian12() {
   apt -y install ruby-rubygems make rpm git snapd curl binutils
   curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
   mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  install /opt/golang/go/bin/go /usr/local/bin/
   gem install fpm
 }
 
@@ -14,6 +15,7 @@ function install_deps_debian11() {
   apt -y install ruby-rubygems make rpm git snapd curl binutils
   curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
   mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  install /opt/golang/go/bin/go /usr/local/bin/
   gem install fpm
 }
 
@@ -22,6 +24,7 @@ function install_deps_ubuntu20.04() {
   apt -y install ruby make rpm git snapd curl binutils
   curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
   mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  install /opt/golang/go/bin/go /usr/local/bin/
   gem install fpm
 }
 
@@ -29,6 +32,7 @@ function install_deps_ubuntu22.04() {
   apt -y install ruby-rubygems make rpm git snapd curl binutils
   curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
   mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  install /opt/golang/go/bin/go /usr/local/bin/
   gem install fpm
 }
 
@@ -36,10 +40,15 @@ function install_deps_ubuntu24.04() {
   apt -y install ruby-rubygems make rpm git snapd curl binutils
   curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
   mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  install /opt/golang/go/bin/go /usr/local/bin/
   gem install fpm
 }
 
 function install_deps_redhat-ubi9() {
   microdnf -y install ruby rpmdevtools make git
+  curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
+  mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  install /opt/golang/go/bin/go /usr/local/bin/
+  install /opt
   gem install fpm
 }
