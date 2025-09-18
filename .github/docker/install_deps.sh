0729ac7586861a3421226d172592cf12892de24f
grant <grant@iowntheinter.net>
2025-09-18 23:54:26 +0000

    matrix amazon 2023 debian 13 redhat 8

--- .github/docker/install_deps.sh
+++ .github/docker/install_deps.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
-VERSION=$(git rev-parse HEAD | cut -c -8)
 
-function install_deps_debian12() {
-  apt -y install ruby-rubygems make rpm git snapd curl binutils
 
-  if [ "$(uname -m)" = "x86_64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
-  elif [ "$(uname -m)" = "aarch64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
-  else
-      echo "unknown arch $(uname -m)"
-      exit 1
-  fi
-  install /opt/golang/go/bin/go /usr/local/bin/
-  gem install fpm
-}
 
 
 
@@ -37,6 +21,39 @@ function install_deps_debian11() {
   gem install fpm
 }
 
+function install_deps_debian12() {
+  apt -y install ruby-rubygems make rpm git snapd curl binutils
+
+  if [ "$(uname -m)" = "x86_64" ]; then
+      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  elif [ "$(uname -m)" = "aarch64" ]; then
+      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+  else
+      echo "unknown arch $(uname -m)"
+      exit 1
+  fi
+  install /opt/golang/go/bin/go /usr/local/bin/
+  gem install fpm
+}
+
+function install_deps_debian13() {
+  apt -y install ruby-rubygems make rpm git snapd curl binutils
+
+  if [ "$(uname -m)" = "x86_64" ]; then
+      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  elif [ "$(uname -m)" = "aarch64" ]; then
+      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+  else
+      echo "unknown arch $(uname -m)"
+      exit 1
+  fi
+  install /opt/golang/go/bin/go /usr/local/bin/
+  gem install fpm
+}
 
 function install_deps_ubuntu20.04() {
   apt -y install ruby make rpm git snapd curl binutils
@@ -89,7 +106,42 @@ function install_deps_ubuntu24.04() {
   gem install fpm
 }
 
-function install_deps_redhat-ubi9() {
+function install_deps_redhat-el8() {
+  microdnf -y install ruby rpmdevtools make git
+
+  if [ "$(uname -m)" = "x86_64" ]; then
+      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  elif [ "$(uname -m)" = "aarch64" ]; then
+      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+  else
+      echo "unknown arch $(uname -m)"
+      exit 1
+  fi
+  install /opt/golang/go/bin/go /usr/local/bin/
+  gem install fpm
+}
+
+function install_deps_redhat-el9() {
+  microdnf -y install ruby rpmdevtools make git
+
+  if [ "$(uname -m)" = "x86_64" ]; then
+      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  elif [ "$(uname -m)" = "aarch64" ]; then
+      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+  else
+      echo "unknown arch $(uname -m)"
+      exit 1
+  fi
+  install /opt/golang/go/bin/go /usr/local/bin/
+  gem install fpm
+}
+
+
+function install_deps_redhat-amazon-2023() {
   microdnf -y install ruby rpmdevtools make git
 
   if [ "$(uname -m)" = "x86_64" ]; then
