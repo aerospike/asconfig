68081388dc1ab9e47148b6c954766466af89bbd8
Grant <grant.haywood@aerospike.com>
2025-10-14 06:00:32 +0000

    try pulling tags

--- .github/docker/build_package.sh
+++ .github/docker/build_package.sh
@@ -16,12 +16,16 @@ function build_packages(){
   env
 
   echo "git branch v r status tag"
+
   git branch -v
   git branch -r
   git status
   git tag
   git remote -v
 #  git describe --tags
+
+  git pull origin
+  git tag
 
   echo "build_package.sh version: $(git describe --tags --always)"
   export VERSION=$(git describe --tags --always)
