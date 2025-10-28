13f9097b155d8f19da144a6294134bc69e7016af
Grant Haywood <ghaywood@aerospike.com>
2025-10-28 15:23:26 -0500

    move READMEs to common space

--- .github/packaging/README-test.sh
+++ .github/packaging/README-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #You can execute this README by replacing the following with your email and your JFrog token:
-# JF_USERNAME='ghaywood@aerospike.com' JF_TOKEN='xxxxxxxxxxxxxxxxxx' .github/packaging/common/test/README.sh
+# JF_USERNAME='ghaywood@aerospike.com' JF_TOKEN='xxxxxxxxxxxxxxxxxx' .github/packaging/common/test/README-test.sh
 
 
 
