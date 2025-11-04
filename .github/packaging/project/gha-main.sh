a7aacd58ba98f48cbb5b88e358e0f4aa2c9aa4bb
Grant Haywood <ghaywood@aerospike.com>
2025-11-04 14:43:49 -0600

    pass distro as arg to main

--- .github/packaging/project/gha-main.sh
+++ .github/packaging/project/gha-main.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
+DISTRO="$1"
 env
 cd local
 git fetch --unshallow --tags --no-recurse-submodules
 git submodule update --init
 ls -laht
-echo ref_name ${{ github.ref_name }}
 git branch -v
-.github/packaging/common/entrypoint.sh -c -d ${{ matrix.distro }}
-.github/packaging/common/entrypoint.sh -e -d ${{ matrix.distro }}
+.github/packaging/common/entrypoint.sh -c -d $DISTRO
+.github/packaging/common/entrypoint.sh -e -d $DISTRO
 ls -laht ../dist
