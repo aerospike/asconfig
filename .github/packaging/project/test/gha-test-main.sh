14333fd094bc48e4b8bae12c1f9764defe882416
Grant Haywood <ghaywood@aerospike.com>
2025-11-05 18:29:19 -0600

    fix typo

--- .github/packaging/project/test/gha-test-main.sh
+++ .github/packaging/project/test/gha-test-main.sh
@@ -4,4 +4,4 @@ DISTRO="$1"
 REPO_NAME="$2"
 env
 git fetch --unshallow --tags --no-recurse-submodules
-github/packaging/common/example-test.sh "$DISTRO" "$REPO_NAME"
+.github/packaging/common/example-test.sh "$DISTRO" "$REPO_NAME"
