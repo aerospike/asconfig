717c8d250c7bd62bc5eac1d9f718d4f656995fdb
Grant Haywood <ghaywood@aerospike.com>
2025-11-21 19:46:03 -0600

    SERVER-216

---
+++ .github/packaging/project/test/gha-test-main.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+DISTRO="$1"
+REPO_NAME="$2"
+git fetch --unshallow --tags --no-recurse-submodules
+.github/packaging/common/example-test.sh "$DISTRO" "$REPO_NAME"
