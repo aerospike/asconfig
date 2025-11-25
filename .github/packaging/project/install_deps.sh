e542895a0c5664e98f8b438860691f2e0be56cdb
Grant Haywood <ghaywood@aerospike.com>
2025-11-25 17:11:52 -0600

    changes for a-spikers review(forgot to push this morning)

--- .github/packaging/project/install_deps.sh
+++ .github/packaging/project/install_deps.sh
@@ -1,106 +1,107 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
-
+export GOLANG_VERSION="1.24.6"
+export FPM_VERSION="1.17.0"
 function install_deps_debian11() {
   apt -y install ruby-rubygems make rpm git snapd curl binutils
 
   if [ "$(uname -m)" = "x86_64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-amd64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz -C /opt/golang
   elif [ "$(uname -m)" = "aarch64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-arm64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz -C /opt/golang
   else
       echo "unknown arch $(uname -m)"
       exit 1
   fi
   install /opt/golang/go/bin/go /usr/local/bin/
-  gem install fpm -v 1.17.0
+  gem install fpm -v "$FPM_VERSION"
 }
 
 function install_deps_debian12() {
   apt -y install ruby-rubygems make rpm git snapd curl binutils
 
   if [ "$(uname -m)" = "x86_64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-amd64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz -C /opt/golang
   elif [ "$(uname -m)" = "aarch64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-arm64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz -C /opt/golang
   else
       echo "unknown arch $(uname -m)"
       exit 1
   fi
   install /opt/golang/go/bin/go /usr/local/bin/
-  gem install fpm -v 1.17.0
+  gem install fpm -v "$FPM_VERSION"
 }
 
 function install_deps_debian13() {
   apt -y install ruby-rubygems make rpm git snapd curl binutils
 
   if [ "$(uname -m)" = "x86_64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-amd64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz -C /opt/golang
   elif [ "$(uname -m)" = "aarch64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-arm64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz -C /opt/golang
   else
       echo "unknown arch $(uname -m)"
       exit 1
   fi
   install /opt/golang/go/bin/go /usr/local/bin/
-  gem install fpm -v 1.17.0
+  gem install fpm -v "$FPM_VERSION"
 }
 
 function install_deps_ubuntu20.04() {
   apt -y install ruby make rpm git snapd curl binutils
 
   if [ "$(uname -m)" = "x86_64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-amd64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz -C /opt/golang
   elif [ "$(uname -m)" = "aarch64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-arm64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz -C /opt/golang
   else
       echo "unknown arch $(uname -m)"
       exit 1
   fi
   install /opt/golang/go/bin/go /usr/local/bin/
-  gem install fpm -v 1.17.0
+  gem install fpm -v "$FPM_VERSION"
 }
 
 function install_deps_ubuntu22.04() {
   apt -y install ruby-rubygems make rpm git snapd curl binutils
 
   if [ "$(uname -m)" = "x86_64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-amd64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz -C /opt/golang
   elif [ "$(uname -m)" = "aarch64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-arm64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz -C /opt/golang
   else
       echo "unknown arch $(uname -m)"
       exit 1
   fi
   install /opt/golang/go/bin/go /usr/local/bin/
-  gem install fpm -v 1.17.0
+  gem install fpm -v "$FPM_VERSION"
 }
 
 function install_deps_ubuntu24.04() {
   apt -y install ruby-rubygems make rpm git snapd curl binutils
 
   if [ "$(uname -m)" = "x86_64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-amd64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz -C /opt/golang
   elif [ "$(uname -m)" = "aarch64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-arm64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz -C /opt/golang
   else
       echo "unknown arch $(uname -m)"
       exit 1
   fi
   install /opt/golang/go/bin/go /usr/local/bin/
-  gem install fpm -v 1.17.0
+  gem install fpm -v "$FPM_VERSION"
 }
 
 function install_deps_el8() {
@@ -109,66 +110,66 @@ function install_deps_el8() {
   gem install --no-document fpm
 
   if [ "$(uname -m)" = "x86_64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-amd64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz -C /opt/golang
   elif [ "$(uname -m)" = "aarch64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-arm64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz -C /opt/golang
   else
       echo "unknown arch $(uname -m)"
       exit 1
   fi
   install /opt/golang/go/bin/go /usr/local/bin/
-  gem install fpm -v 1.17.0
+  gem install fpm -v "$FPM_VERSION"
 }
 
 function install_deps_el9() {
   dnf -y install ruby rpmdevtools make git
 
   if [ "$(uname -m)" = "x86_64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-amd64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz -C /opt/golang
   elif [ "$(uname -m)" = "aarch64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-arm64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz -C /opt/golang
   else
       echo "unknown arch $(uname -m)"
       exit 1
   fi
   install /opt/golang/go/bin/go /usr/local/bin/
-  gem install fpm -v 1.17.0
+  gem install fpm -v "$FPM_VERSION"
 }
 
 function install_deps_el10() {
   dnf -y install ruby rpmdevtools make git
 
   if [ "$(uname -m)" = "x86_64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-amd64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz -C /opt/golang
   elif [ "$(uname -m)" = "aarch64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-arm64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz -C /opt/golang
   else
       echo "unknown arch $(uname -m)"
       exit 1
   fi
   install /opt/golang/go/bin/go /usr/local/bin/
-  gem install fpm -v 1.17.0
+  gem install fpm -v "$FPM_VERSION"
 }
 
 function install_deps_amzn2023() {
   dnf -y install ruby rpmdevtools make git
 
   if [ "$(uname -m)" = "x86_64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-amd64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-amd64.tar.gz -C /opt/golang
   elif [ "$(uname -m)" = "aarch64" ]; then
-      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
-      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+      curl -L https://go.dev/dl/go"$GOLANG_VERSION".linux-arm64.tar.gz -o /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go"$GOLANG_VERSION".linux-arm64.tar.gz -C /opt/golang
   else
       echo "unknown arch $(uname -m)"
       exit 1
   fi
   install /opt/golang/go/bin/go /usr/local/bin/
-  gem install fpm -v 1.17.0
+  gem install fpm -v "$FPM_VERSION"
 }
